39a94d619b88b8b316acc1c52c611ae3dcc72330
Chris Arriola <arriolac279@gmail.com>
2016-05-29 14:01:38 -0700

    Step 4.

--- app/src/main/java/com/codepath/githubrxjava/MainActivity.java
+++ app/src/main/java/com/codepath/githubrxjava/MainActivity.java
@@ -1,12 +1,63 @@
 package com.codepath.githubrxjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import java.util.List;
+import rx.Observer;
+import rx.Subscription;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private GitHubRepoAdapter adapter = new GitHubRepoAdapter();
+    private Subscription subscription;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView listView = (ListView) findViewById(R.id.list_view_repos);
+        listView.setAdapter(adapter);
+
+        final EditText editTextUsername = (EditText) findViewById(R.id.edit_text_username);
+        final Button buttonSearch = (Button) findViewById(R.id.button_search);
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                final String username = editTextUsername.getText().toString();
+                if (!TextUtils.isEmpty(username)) {
+                    getStarredRepos(username);
+                }
+            }
+        });
+    }
+
+    @Override protected void onDestroy() {
+        if (subscription != null && !subscription.isUnsubscribed()) {
+            subscription.unsubscribe();
+        }
+        super.onDestroy();
+    }
+
+    private void getStarredRepos(String username) {
+        subscription = GitHubClient.getInstance().getStarredRepos(username).subscribe(new Observer<List<GitHubRepo>>() {
+            @Override public void onCompleted() {
+                Log.d(TAG, "In onCompleted()");
+            }
+
+            @Override public void onError(Throwable e) {
+                Log.d(TAG, "In onError()");
+            }
+
+            @Override public void onNext(List<GitHubRepo> gitHubRepos) {
+                Log.d(TAG, "In onNext()");
+                adapter.setGitHubRepos(gitHubRepos);
+            }
+        });
     }
 }
