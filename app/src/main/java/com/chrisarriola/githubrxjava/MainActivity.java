9340f5daf85c9ae379ca88eb9a02361936907a2f
Manas Chaudhari <chaudhari.manas@gmail.com>
2016-09-05 12:31:53 +0530

    Remove adapters and activity boilerplate

--- app/src/main/java/com/chrisarriola/githubrxjava/MainActivity.java
+++ app/src/main/java/com/chrisarriola/githubrxjava/MainActivity.java
@@ -1,70 +1,20 @@
 package com.chrisarriola.githubrxjava;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import java.util.List;
-import rx.Observer;
-import rx.Subscription;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
+import android.support.annotation.NonNull;
 
-public class MainActivity extends AppCompatActivity {
+import com.manaschaudhari.android_mvvm.MvvmActivity;
+import com.manaschaudhari.android_mvvm.ViewModel;
 
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private GitHubRepoAdapter adapter = new GitHubRepoAdapter();
-    private Subscription subscription;
+public class MainActivity extends MvvmActivity {
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final ListView listView = (ListView) findViewById(R.id.list_view_repos);
-        listView.setAdapter(adapter);
-
-        final EditText editTextUsername = (EditText) findViewById(R.id.edit_text_username);
-        final Button buttonSearch = (Button) findViewById(R.id.button_search);
-        buttonSearch.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                final String username = editTextUsername.getText().toString();
-                if (!TextUtils.isEmpty(username)) {
-                    getStarredRepos(username);
-                }
-            }
-        });
+    @NonNull
+    @Override
+    public ViewModel createViewModel() {
+        return new MainViewModel();
     }
 
-    @Override protected void onDestroy() {
-        if (subscription != null && !subscription.isUnsubscribed()) {
-            subscription.unsubscribe();
-        }
-        super.onDestroy();
-    }
-
-    private void getStarredRepos(String username) {
-        subscription = GitHubClient.getInstance()
-                                   .getStarredRepos(username)
-                                   .subscribeOn(Schedulers.io())
-                                   .observeOn(AndroidSchedulers.mainThread())
-                                   .subscribe(new Observer<List<GitHubRepo>>() {
-                                       @Override public void onCompleted() {
-                                           Log.d(TAG, "In onCompleted()");
-                                       }
-
-                                       @Override public void onError(Throwable e) {
-                                           e.printStackTrace();
-                                           Log.d(TAG, "In onError()");
-                                       }
-
-                                       @Override public void onNext(List<GitHubRepo> gitHubRepos) {
-                                           Log.d(TAG, "In onNext()");
-                                           adapter.setGitHubRepos(gitHubRepos);
-                                       }
-                                   });
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
     }
 }
